4b877bb1ccd769e6a3ed32136866fbbcb4a5eb7f
youngin135 <laykim2023@naver.com>
2024-07-28 11:14:58 +0100

    [chore] : DB database setting

---
+++ NewFace/Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewFace.Data;
+
+namespace NewFace.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        private readonly DataContext _context;
+
+        public AuthController(DataContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
