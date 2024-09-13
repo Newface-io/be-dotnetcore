9124683242fa80c1b3adf7e8d5ae10dab50df451
LayKim2 <102665441+LayKim2@users.noreply.github.com>
2024-09-13 16:44:33 +0900

    +

---
+++ NewFace/Migrations/20240913074016_updateActorImageForMainImage.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NewFace.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateActorImageForMainImage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsMainImage",
+                table: "ActorImages",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsMainImage",
+                table: "ActorImages");
+        }
+    }
+}
